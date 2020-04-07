9ffa7df637c7ac775871cfe2f1c0219d73658d68
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-07 05:42:22 +0530

    Dummy message

--- Examples/01-var.tf
+++ Examples/01-var.tf
@@ -10,6 +10,24 @@ variable "NUMBER" {
   default = 100
 }
 
+variable "LIST" {
+  default = ["apple","Mango"]
+}
+
+variable "MAP" {
+  default = {
+    "red" ="apple",
+    "yellow" ="Mango"}
+}
+
+output "MAP" {
+  value = var.MAP["red"]
+}
+
+output "LIST" {
+  value = var.LIST[0]
+}
+
 output "NAME" {
   value = var.NAME
 }
