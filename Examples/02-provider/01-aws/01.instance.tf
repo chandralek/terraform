b03c0d670e224c3dbd0cd134c59dd2305315e5ff
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-07 20:00:37 +0530

    Dummy message

--- Examples/02-provider/01-aws/01.instance.tf
+++ Examples/02-provider/01-aws/01.instance.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "sample" {
   ami = "ami-0777ff5c030fe1d38"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-08a40e4443cb4ef02"]
 }
