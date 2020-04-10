07898233331cd3cafe6c34952eff1ed1d7e5e853
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-10 20:50:37 +0530

    Dummy message

---
+++ Examples/08-remote-state/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "tfstates-demo"
+    key    = "sample/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_instance" "sample" {
+  ami = "ami-0777ff5c030fe1d38"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-08a40e4443cb4ef02"]
+}
